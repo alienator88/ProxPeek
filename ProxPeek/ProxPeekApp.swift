48e7eb0c4aa78dd216a45176441b1dccf93b3093
Alin Lupascu <lupascu.marius@gmail.com>
2024-11-14 11:52:47 -0700

    Initial Commit

---
+++ ProxPeek/ProxPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxPeekApp.swift
+//  ProxPeek
+//
+//  Created by Alin Lupascu on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ProxPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
